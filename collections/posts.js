cb619190e474219e09c2df3243dba9a87b52ee58
anna <annarschneider@gmail.com>
2013-12-14 17:30:52 -0800

    use Method to submit posts

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,34 @@
 Posts = new Meteor.Collection('posts');
+
+Meteor.methods({
+  post: function(postAttributes) {
+      // set vars
+      var user = Meteor.user(),
+	postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+      // check for logged in
+      if (!user)
+	throw new Meteor.Error(401, "log in please");
+
+      // check for title
+      if (!postAttributes.title)
+	throw new Meteor.Error(422, "need a title");
+
+      // check for repeats, 302 = redirect
+      if (postAttributes.url && postWithSameLink) {
+	throw new Meteor.Error(302, 'this link has already been posted',
+			       postWithSameLink._id);
+      }
+
+      // pick out whitelisted keys
+      var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+	userId: user._id,
+	    author: user.username,
+	    submitted: new Date().getTime()
+      });
+
+      // actually insert
+      var postId = Posts.insert(post);
+      return postId;
+  }
+});
